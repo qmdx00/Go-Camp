2c208df3d5d72055dbd71ef225ef1858a3eaddd6
wimi <ywm00@qq.com>
2021-07-22 16:14:45 +0800

    feat(week09): code WIP

---
+++ week09/decoder.go
@@ -0,0 +1,8 @@
+package main
+
+
+// Decoder for goim packet
+// https://github.com/Terry-Mao/goim/blob/e742c99ad76e626d5f6df8b33bc47ca005501980/api/protocol/protocol.go#L25
+type Decoder struct {
+	// Todo
+}
